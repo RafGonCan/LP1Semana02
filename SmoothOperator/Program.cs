d78f50d53eaf302014ee45b36db9491722eda946
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-27 21:33:47 +0000

    Exercicio 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Show message
+            Console.WriteLine("Insere número inteiro:");
+            //User input
+            string num = Console.ReadLine();
+            //Convert input into sbyte
+            sbyte n = Convert.ToSByte(num);
+            //decrement
+            n--;
+            //show result
+            Console.WriteLine(n);
+            //increment
+            n++;
+            //show result
+            Console.WriteLine(n);
+        }
+    }
+}
