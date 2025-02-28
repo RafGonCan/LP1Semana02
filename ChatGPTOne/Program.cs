68c354648161e20191c90a2694c6275a5e4ba8ad
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-28 02:38:37 +0000

    declare variables and start of the cicle while

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //declare string variables
+            string question, response;
+
+            //Start of the cicle
+            while (true)
+            {
+                //starting question
+                Console.WriteLine("What is your question?");
+                question = Console.ReadLine();
+
+                
+            }
+        }
+    }
+}
