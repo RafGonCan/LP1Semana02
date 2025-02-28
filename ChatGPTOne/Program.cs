ad68a6c3a0eca1533c7a3ef4bdbf9df6b4f45af0
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-28 02:54:53 +0000

    Q&A for the bot

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -16,7 +16,32 @@ namespace ChatGPTOne
                 Console.WriteLine("What is your question?");
                 question = Console.ReadLine();
 
-                
+                //Q&A
+                switch (question)
+                {
+                    case "Who are you?":
+                        response = "The best AI you ever known!";
+                        Console.WriteLine(response);
+                    break;
+                    case "how old are you?":
+                        response = "I'm 5000 years old!!!";
+                        Console.WriteLine(response);
+                    break;
+                    case "What is your name?":
+                        response = "Jeremy";
+                        Console.WriteLine(response);
+                    break;
+                    case "Are you smart?":
+                        response = "...";
+                        Console.WriteLine(response);
+                    break;
+                    case "EXIT":
+                    break;
+                    default:
+                        response = "Don't understand the question...";
+                        Console.WriteLine(response);
+                        break;
+                }
             }
         }
     }
