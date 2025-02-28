4c07a9bbbf7c8a0a403a80c0a83931f8ec6854fa
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-28 17:49:04 +0000

    while cicle

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text.Json.Serialization.Metadata;
 
 namespace GuessTheNumber
 {
@@ -9,21 +11,41 @@ namespace GuessTheNumber
             Random rnd = new Random();
             int numberToGuess = rnd.Next(31);
             
+            int tries = 0;
             //Message for input
             Console.WriteLine("Insert number:");
             string number = Console.ReadLine();
-            
+
             //Convert
             int num = Convert.ToInt32(number);
+            tries++;
 
-            //condition
-            if (num < 0)
+            while (num != numberToGuess)
             {
-                Console.WriteLine("Number is to low");
-            }
-            if (num > 30)
-            {
-                Console.WriteLine("Number is to high");
+                //condition
+                if (num < 0 || num > 30)
+                {
+                    Console.WriteLine("Number must be between 0 and 30. Try again");
+                    break;
+                }
+                
+                if (num < numberToGuess)
+                {
+                    Console.WriteLine($"The hidden number is lower than {num}. Try again");
+                    break;
+                }
+
+                if (num > numberToGuess)
+                {
+                    Console.WriteLine($"The hidden number is higher than {num}. Try again");
+                    break;
+                }
+
+                if (num == numberToGuess)
+                {
+                    Console.WriteLine($"You fonud the hidden number {numberToGuess} after {tries} tries");
+                    break;
+                }
             }
         }
     }
