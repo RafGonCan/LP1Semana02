4f939df0e376d0d96ffb089ff2e0b6278930d2a6
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-28 17:14:51 +0000

    Input, convert and condition

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int numberToGuess = rnd.Next(31);
+            
+            //Message for input
+            Console.WriteLine("Insert number:");
+            string number = Console.ReadLine();
+            
+            //Convert
+            int num = Convert.ToInt32(number);
+
+            //condition
+            if (num < 0)
+            {
+                Console.WriteLine("Number is to low");
+            }
+            if (num > 30)
+            {
+                Console.WriteLine("Number is to high");
+            }
+        }
+    }
+}
