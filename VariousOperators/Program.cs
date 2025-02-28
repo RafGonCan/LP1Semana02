6d80d1375a895921768730f07cf243072505f57a
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-28 00:48:07 +0000

    Exercicio 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Show message
+            Console.WriteLine("Insere número inteiro-não negativo:");
+            //User input
+            string num = Console.ReadLine();
+            //Convert input into byte
+            byte n = Convert.ToByte(num);
+            //calculations
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^6);
+            Console.WriteLine(n>10);
+        }
+    }
+}
