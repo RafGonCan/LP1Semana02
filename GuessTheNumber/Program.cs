7bf379e2c0292ba7f88234689fb2b851edda2115
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-06 20:22:01 +0000

    correção de erros

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,42 +10,39 @@ namespace GuessTheNumber
         {
             Random rnd = new Random();
             int numberToGuess = rnd.Next(31);
-            
             int tries = 0;
+            bool guess = true;
             
-            //Message for input
-            Console.WriteLine("Insert number:");
-            string number = Console.ReadLine();
 
-            //Convert
-            int num = Convert.ToInt32(number);
-
-            while (num != numberToGuess)
+            while (guess)
             {
-            tries++;
+                //Message for input
+                Console.WriteLine("Insert number:");
+                string number = Console.ReadLine();
 
-                //condition
+                //Convert
+                int num = Convert.ToInt32(number);
+                tries++;
+
                 if (num < 0 || num > 30)
                 {
                     Console.WriteLine("Number must be between 0 and 30. Try again");
-                    break;
                 }
                 
-                if (num < numberToGuess)
+                else if (num > numberToGuess)
                 {
                     Console.WriteLine($"The hidden number is lower than {num}. Try again");
-                    break;
                 }
 
-                if (num > numberToGuess)
+                else if (num < numberToGuess)
                 {
                     Console.WriteLine($"The hidden number is higher than {num}. Try again");
-                    break;
                 }
 
-                if (num == numberToGuess)
+                else if (num == numberToGuess)
                 {
                     Console.WriteLine($"You found the hidden number {numberToGuess} after {tries} tries");
+                    guess = false;
                     break;
                 }
             }
